5e4b5d2c7640c1c60f367d49dd1623e6e44e6229
bikash720 <63512238+bikash720@users.noreply.github.com>
2021-06-06 15:05:03 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 # Change input and output details
 
-output_path="bela-re-bela" # Output Path (Same will be pushed in main also)
-input_url="https://sabmalpuri.s3.ap-south-1.amazonaws.com/SAMBALPURI+SONG/Bela+Re+Bela+Return+Full+Studio+Versoin++FTll+Umakant+Barik+%26+Rojalin+sahu+Umakant+Gumsum+Official+(+1080.mp4" # Input direct file url
+output_path="break-up" # Output Path (Same will be pushed in main also)
+input_url="https://sabmalpuri.s3.ap-south-1.amazonaws.com/SAMBALPURI+SONG/Break+Up+(Umakant+Barik)+Sambalpuri+Video+2017+(Copyright+Reserved)++720+X+1280.mp4" # Input direct file url
 input_extension="mp4" # Extension of file url
 
 
