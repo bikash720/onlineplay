b2450d9115a715c5086bca25954703ffec3a1775
bikash720 <63512238+bikash720@users.noreply.github.com>
2021-06-06 14:05:59 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 # Change input and output details
 
-output_path="e-di-e-dil" # Output Path (Same will be pushed in main also)
-input_url="https://sabmalpuri.s3.ap-south-1.amazonaws.com/SAMBALPURI+SONG/Ae+Dil+Ae+Dil+FULL+HD+VIDEO+SONG+II+DEVAA+(Desi+Rowdy)+II+Umakant+gumsum+official+II+(+1080+X+1920+).mp4" # Input direct file url
+output_path="baragada-chaulabara" # Output Path (Same will be pushed in main also)
+input_url="https://sabmalpuri.s3.ap-south-1.amazonaws.com/SAMBALPURI+SONG/Baragadar+Chaula+Bara+_+Bargadia+Mithun+da+_+Leema_+Dillip+_Anamika+_Samabalpuri+Song+_Adarsh+Malhar+(+1080+X+1920+).mp4" # Input direct file url
 input_extension="mp4" # Extension of file url
 
 
