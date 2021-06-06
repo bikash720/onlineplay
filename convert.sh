678218307bd95122fe33f7e13cd4030c274d93ac
bikash720 <63512238+bikash720@users.noreply.github.com>
2021-06-06 21:57:55 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 # Change input and output details
 
-output_path="gelehi" # Output Path (Same will be pushed in main also)
-input_url="https://sabmalpuri.s3.ap-south-1.amazonaws.com/SAMBALPURI+SONG/GELEHI+SONG+!!+Singer+-+Nil+Sagar+%26+Archana+Padhi+!!+Sambalpuri+Song+2020+!!+RIYANSHI+MUSIC+(+1080+X+1920+).mp4" # Input direct file url
+output_path="heart-attack" # Output Path (Same will be pushed in main also)
+input_url="https://sabmalpuri.s3.ap-south-1.amazonaws.com/SAMBALPURI+SONG/Heart+attack+_+Sambalpuri+Music+Video+2021+_+Sandeep+%26+Kajal+_+Suresh+Suna+%26+Lipsa+mahapatra+_+E4U+_+(+1080+X+1920+60fps+).mp4" # Input direct file url
 input_extension="mp4" # Extension of file url
 
 
